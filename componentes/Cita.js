948936ace661ee39b0306a3e3d679afe0ef25b57
Renzo Oldani <oldanirenzo@gmail.com>
2020-09-23 23:35:52 -0300

    Agrego un Touchable para desplazarme por las citas

--- componentes/Cita.js
+++ componentes/Cita.js
@@ -5,10 +5,10 @@ import {
   View,
   TouchableHighlight,
   ToastAndroid,
+  TouchableWithoutFeedback,
 } from 'react-native';
 
 const Cita = ({item, eliminarCliente}) => {
-  
   const dialogoEliminar = (id) => {
     console.log('Eliminando...');
     eliminarCliente(id);
@@ -16,32 +16,34 @@ const Cita = ({item, eliminarCliente}) => {
   };
 
   return (
-    <View style={styles.cita}>
-      <View>
-        <Text style={styles.label}>Cliente</Text>
-        <Text style={styles.texto}>{item.cliente}</Text>
-      </View>
-      <View>
-        <Text style={styles.label}>Teléfono</Text>
-        <Text style={styles.texto}>{item.telefono}</Text>
-      </View>
-      <View>
-        <Text style={styles.label}>Tratamientos</Text>
-        <Text style={styles.texto}>{item.tratamiento}</Text>
-      </View>
-      <View>
-        <Text style={styles.label}>Fecha</Text>
-        <Text style={styles.texto}>{item.fecha}</Text>
-      </View>
+    <TouchableWithoutFeedback>
+      <View style={styles.cita}>
+        <View>
+          <Text style={styles.label}>Cliente</Text>
+          <Text style={styles.texto}>{item.cliente}</Text>
+        </View>
+        <View>
+          <Text style={styles.label}>Teléfono</Text>
+          <Text style={styles.texto}>{item.telefono}</Text>
+        </View>
+        <View>
+          <Text style={styles.label}>Tratamiento</Text>
+          <Text style={styles.texto}>{item.tratamiento}</Text>
+        </View>
+        <View>
+          <Text style={styles.label}>Fecha</Text>
+          <Text style={styles.texto}>{item.fecha}</Text>
+        </View>
 
-      <View>
-        <TouchableHighlight
-          onPress={() => dialogoEliminar(item.id)}
-          style={styles.btnEliminar}>
-          <Text style={styles.textoEliminar}>Eliminar &times;</Text>
-        </TouchableHighlight>
+        <View>
+          <TouchableHighlight
+            onPress={() => dialogoEliminar(item.id)}
+            style={styles.btnEliminar}>
+            <Text style={styles.textoEliminar}>Eliminar &times;</Text>
+          </TouchableHighlight>
+        </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -49,10 +51,13 @@ const styles = StyleSheet.create({
   cita: {
     backgroundColor: '#FFF',
     borderBottomColor: '#e1e1e1',
+    borderRightColor: '#e1e1e1',
     borderStyle: 'solid',
-    borderBottomWidth: 1,
-    paddingVertical: 20, //Si el padding es para arriba y abajo es el mismo, puedo usar paddingVertical
-    paddingHorizontal: 10, //Tambien existe paddingHorizontal
+    borderBottomWidth: 5,
+    borderRightWidth: 5,
+    marginBottom: 20,
+    paddingVertical: 10, //Si el padding es para arriba y abajo es el mismo, puedo usar paddingVertical
+    paddingHorizontal: 20, //Tambien existe paddingHorizontal
   },
   label: {
     fontWeight: 'bold',
